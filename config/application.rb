bd702278a0f606c952f8f7ccb8378ad50ac04101
Alexey Vorobiev <alexey@itima.ru>
2012-01-04 14:42:01 +0400

    added rvmrc and Facebook app api keys

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ end
 module Spreader
   class Application < Rails::Application
 
-    # don't generate RSpec tests for views and helpers
     config.generators do |g|
       g.view_specs false
       g.helper_specs false
